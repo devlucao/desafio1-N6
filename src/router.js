dd47825c218d100a2755ec239fb1e0252b440f8c
Lucas <devlucao@gmail.com>
2022-09-24 16:45:55 -0300

    sobe geral

---
+++ src/router.js
@@ -0,0 +1,29 @@
+export class Router {
+
+  routes = {};
+
+  add(routeName, page) {
+    this.routes[routeName] = page;
+  }
+
+  route(event) {
+    event = event || window.event;
+    
+    window.history.pushState({}, "", event.target.href);
+    event.preventDefault();
+    
+    this.handle();
+  }
+  
+  handle() {
+    const { pathname } = window.location;
+    
+    const route = this.routes[pathname] || this.routes[404];
+  
+    fetch(route)
+      .then((data) => data.text())
+      .then((html) => {
+        document.querySelector("#content").innerHTML = html;
+      });
+  }
+}
